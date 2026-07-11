b97b13ffb3f89e06e6b29eb0dbf18abf966fcf0f
freezedawn <3149419871@qq.com>
2026-07-11 21:51:42 +0800

    Update ledger persistence and CSV import

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "quick-ledger-sample-v3";
+﻿const CACHE_NAME = "quick-ledger-sample-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -12,5 +12,6 @@ self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
+
 
 
